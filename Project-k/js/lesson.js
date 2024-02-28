8fb6e6722e68abe98ae23d7a402885f0fa9fe056
MarselXd-3 <95333397+MarselXd-3@users.noreply.github.com>
2024-02-28 11:57:24 +0600

    Update lesson.js

--- Project-k/js/lesson.js
+++ Project-k/js/lesson.js
@@ -86,3 +86,54 @@ document.addEventListener("DOMContentLoaded", function() {
     convertor(usdInput, somInput, eurInput, "usd");
     convertor(eurInput, somInput, usdInput, "eur");
 });
+
+
+const btnPrev = document.querySelector("#btn-prev");
+const btnNext = document.querySelector("#btn-next");
+const cardBlock = document.querySelector('.card');
+
+let currentCard = 1;
+
+function fetchData(cardNumber) {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${cardNumber}`)
+    .then(response => response.json())
+    .then(data => {
+        const completedText = data.completed ? 'Completed' : 'Not Completed';
+        const completedColor = data.completed ? 'green' : 'red';
+        cardBlock.innerHTML = `
+            <p>${data.title}</p>
+            <p style="color: ${completedColor}">${completedText}</p>
+            <span>${data.id}</span>`;
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+
+function fetchPostsData() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+        console.log("Posts data:", data);
+    })
+    .catch(error => {
+        console.error('Error fetching posts data:', error);
+    });
+}
+
+function showNextCard() {
+    currentCard = currentCard === 200 ? 1 : currentCard + 1;
+    fetchData(currentCard);
+}
+
+function showPrevCard() {
+    currentCard = currentCard === 1 ? 200 : currentCard - 1;
+    fetchData(currentCard);
+}
+
+
+fetchData(currentCard);
+fetchPostsData();
+
+btnPrev.onclick = showPrevCard;
+btnNext.onclick = showNextCard;
